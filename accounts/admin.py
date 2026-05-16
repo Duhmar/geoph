c6bb802134512b8bebe4ae15ce7b3e41ea394de5
Duhmar <damplovechannel@gmail.com>
2026-05-16 23:28:29 +0800

    Removed hotels and added Cloudinary image storage

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,15 +1,11 @@
 from django.contrib import admin
-from .models import Report, ReportMedia, Profile, Comment, Hotel, RoomBooking, Province, Accommodation, TouristSpot, TransportationTerminal
+from .models import Report, ReportMedia, Profile, Comment, Province, Accommodation, TouristSpot, TransportationTerminal
 
 # Register your models here.
 admin.site.register(Report)
 admin.site.register(ReportMedia)
 admin.site.register(Profile)
 admin.site.register(Comment)
-admin.site.register(Hotel)
-admin.site.register(RoomBooking)
-
-# The new GeoPH models
 admin.site.register(Province)
 admin.site.register(Accommodation)
 admin.site.register(TouristSpot)
